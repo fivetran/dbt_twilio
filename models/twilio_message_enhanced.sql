5271240051e4be3762e0a9a99f0d2fec796da570
fivetran-reneeli <renee.li@fivetran.com>
2023-06-21 19:29:44 -0400

    updates

--- models/twilio_message_enhanced.sql
+++ models/twilio_message_enhanced.sql
@@ -6,41 +6,50 @@ with messages as (
 
 inbound_messages as (
 
-    select 
-        message_from as phone_number,
-        status,
-        date_sent,
-        message_from,
-        message_to,
+    select
+        account_id,
         body,
+        created_at,
+        date_sent,
+        direction,
         error_code,
         error_message,
-        direction,
+        message_from as phone_number,
+        message_id,
+        messaging_service_id,
         num_media,
-        num_segments
+        num_segments,
+        price,
+        price_unit,
+        status,
+        message_to,
+        updated_at
 
     from messages
-    group by 1,2
     where direction like '%inbound%'
 ),
 
 outbound_messages as (
 
-    select 
-        message_from as phone_number,
-        status,
-        date_sent,
-        message_from,
-        message_to,
+    select
+        account_id,
         body,
+        created_at,
+        date_sent,
+        direction,
         error_code,
         error_message,
-        direction,
+        message_to as phone_number,
+        message_id,
+        messaging_service_id,
         num_media,
-        num_segments
-
+        num_segments,
+        price,
+        price_unit,
+        status,
+        message_from,
+        updated_at
     from messages
-    group by 1,2
     where direction like '%outbound%'
 ),
 
@@ -49,12 +58,12 @@ union_messages as (
     select * from inbound_messages
     union all
     select * from outbound_messages
-)
+),
 
 incoming_phone_number as (
 
     select *
-    from {{ var('income_phone_number')}}
+    from {{ var('incoming_phone_number')}}
 ),
 
 addresses as (
@@ -65,36 +74,37 @@ addresses as (
 
 final as (
 
-    select 
+    select
+        union_messages.message_id,
+        union_messages.messaging_service_id,
         union_messages.date_sent,
-        union_messages.date_sent as week_sent, -- date trunc week
-        union_messages.date_sent as month_sent, -- date trunc month
-        union_messages.message_from,
-        union_messages.message_to,
+        cast ({{ dbt.date_trunc("week","union_messages.date_sent") }} as date) as week_sent,
+        cast ({{ dbt.date_trunc("month","union_messages.date_sent") }} as date) as month_sent,
+        union_messages.account_id,
+        union_messages.created_at,
+        union_messages.direction,
+        union_messages.phone_number,
         union_messages.body,
         union_messages.status,
         union_messages.error_code,
         union_messages.error_message,
-        union_messages.direction,
         union_messages.num_media,
         union_messages.num_segments,
+        union_messages.price,
+        union_messages.price_unit,
+        union_messages.updated_at,
         addresses.iso_country
-
-
-
 
     from union_messages
 
     left join incoming_phone_number
-        on union_messages.message_to = incoming_phone_number.phone_number
-        on union_messages.message_from = incoming_phone_number.phone_number
+        on union_messages.phone_number = incoming_phone_number.phone_number
+        and union_messages.phone_number = incoming_phone_number.phone_number
 
     left join addresses
-        on income_phone_number.address_id = addresses.address_id
-
+        on incoming_phone_number.address_id = addresses.address_id
 )
 
-
-select * 
+select *
 from final
 
--- models/twilio_number_overview.sql
+++ models/twilio_number_overview.sql
@@ -1,58 +1,69 @@
--- update to pull from message_enhanced
-
-with messages as (
+with message_enhanced as (
 
     select *
-    from {{ var('message')}}
-),
-
-inbound_messages as (
-
-    select 
-        message_from as phone_number,
-        status,
-        count(message_id) as total_messages,
-        sum(price) as total_spent
-
-    from messages
-    group by 1,2
-    where direction like '%inbound%'
-),
-
-outbound_messages as (
-
-    select 
-        message_to as phone_number,
-        status,
-        count(message_id) as total_messages,
-        sum(price) as total_spent
-
-    from messages
-    group by 1,2
-    where direction like '%outbound%'
-),
-
-final as (
-
-    select * from inbound_messages
-    union all
-    select * from outbound_messages
+    from {{ ref('twilio_message_enhanced')}}
 )
 
-select * 
-from final
+select 
+    phone_number,
+    case
+        when direction like '%outbound%'
+        then count(message_id)
+        end as total_outbound_messages,
+    case
+        when direction like '%inbound%'
+        then count(message_id)
+        end as total_inbound_messages,
+    case 
+        when status like '%accepted%'
+        then count(message_id)
+        end as total_accepted_messages,
+    case 
+        when status like '%scheduled%'
+        then count(message_id)
+        end as total_scheduled_messages,
+    case 
+        when status like '%canceled%'
+        then count(message_id)
+        end as total_canceled_messages,
+    case 
+        when status like '%queued%'
+        then count(message_id)
+        end as total_queued_messages,
+    case 
+        when status like '%sending%'
+        then count(message_id)
+        end as total_sending_messages,
+    case
+        when status like '%sent%'
+        then count(message_id)
+        end as total_sent_messages,
+    case 
+        when status like '%failed%'
+        then count(message_id)
+        end as total_failed_messages,
+    case 
+        when status like '%delivered%'
+        then count(message_id)
+        end as total_delivered_messages,
+    case 
+        when status like '%undelivered%'
+        then count(message_id)
+        end as total_undelivered_messages,
+    case 
+        when status like '%receiving%'
+        then count(message_id)
+        end as total_receiving_messages,
+    case 
+        when status like '%received%'
+        then count(message_id)
+        end as total_received_messages,
+    case 
+        when status like '%read%'
+        then count(message_id)
+        end as total_read_messages,
+    count(message_id) as total_messages,
+    sum(price) as total_spent
 
-
--- Total Outgoing Messages
--- Total Messages Sent
--- Total Delivered
--- Total Undelivered 
--- Total Failed 
--- Total Spent
--- Programmable SMS Spend
--- SMS Transactions 
--- Total Number of Responses
--- Current Opt Status 
---      - Are they Opt In or Opt Out
--- Opted out date 
---      - Null if they are not opted out
+from message_enhanced
+group by phone_number, direction, status
