10fa63e13e62298521c9bccac30188b071ae7d10
fivetran-reneeli <renee.li@fivetran.com>
2023-06-22 11:37:42 -0400

    fix number overview model

--- models/twilio_number_overview.sql
+++ models/twilio_number_overview.sql
@@ -4,66 +4,66 @@ with message_enhanced as (
     from {{ ref('twilio_message_enhanced')}}
 )
 
-select 
+select
     phone_number,
-    case
+    count(case
         when direction like '%outbound%'
-        then count(message_id)
-        end as total_outbound_messages,
-    case
+        then message_id end)
+        as total_outbound_messages,
+    count(case
         when direction like '%inbound%'
-        then count(message_id)
-        end as total_inbound_messages,
-    case 
+        then message_id end)
+        as total_inbound_messages,
+    count(case 
         when status like '%accepted%'
-        then count(message_id)
-        end as total_accepted_messages,
-    case 
+        then message_id end)
+        as total_accepted_messages,
+    count(case
         when status like '%scheduled%'
-        then count(message_id)
-        end as total_scheduled_messages,
-    case 
+        then message_id end)
+        as total_scheduled_messages,
+    count(case 
         when status like '%canceled%'
-        then count(message_id)
-        end as total_canceled_messages,
-    case 
+        then message_id end)
+        as total_canceled_messages,
+    count(case 
         when status like '%queued%'
-        then count(message_id)
-        end as total_queued_messages,
-    case 
+        then message_id end)
+        as total_queued_messages,
+    count(case 
         when status like '%sending%'
-        then count(message_id)
-        end as total_sending_messages,
-    case
+        then message_id end)
+        as total_sending_messages,
+    count(case
         when status like '%sent%'
-        then count(message_id)
-        end as total_sent_messages,
-    case 
+        then message_id end)
+        as total_sent_messages,
+    count(case 
         when status like '%failed%'
-        then count(message_id)
-        end as total_failed_messages,
-    case 
-        when status like '%delivered%'
-        then count(message_id)
-        end as total_delivered_messages,
-    case 
-        when status like '%undelivered%'
-        then count(message_id)
-        end as total_undelivered_messages,
-    case 
+        then message_id end)
+        as total_failed_messages,
+    count(case 
+        when status like 'delivered'
+        then message_id end)
+        as total_delivered_messages,
+    count(case 
+        when status like 'undelivered'
+        then message_id end)
+        as total_undelivered_messages,
+    count(case 
         when status like '%receiving%'
-        then count(message_id)
-        end as total_receiving_messages,
-    case 
+        then message_id end)
+        as total_receiving_messages,
+    count(case 
         when status like '%received%'
-        then count(message_id)
-        end as total_received_messages,
-    case 
+        then message_id end)
+        as total_received_messages,
+    count(case 
         when status like '%read%'
-        then count(message_id)
-        end as total_read_messages,
+        then message_id end)
+        as total_read_messages,
     count(message_id) as total_messages,
     sum(price) as total_spent
 
 from message_enhanced
-group by phone_number, direction, status
+group by phone_number
