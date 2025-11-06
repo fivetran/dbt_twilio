50ebdc54315d8df0b7b67902e4cfcd957e19d51e
Joe Markiewicz <74217849+fivetran-joemarkiewicz@users.noreply.github.com>
2025-11-06 17:19:56 -0600

    Apply suggestions from code review
    Co-authored-by: fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>

--- integration_tests/tests/consistency_twilio__number_overview.sql
+++ integration_tests/tests/consistency_twilio__number_overview.sql
@@ -3,7 +3,7 @@
     enabled=var('fivetran_validation_tests_enabled', false)
 ) }}
 
-{% set exclude_cols = var('consistency_test_exclude_metrics', []) %}
+{% set exclude_cols = ['total_spend'] + var('consistency_test_exclude_metrics', []) %}
 
 -- this test ensures the twilio__number_overview end model matches the prior version
 with prod as (
